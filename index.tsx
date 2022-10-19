5728dfef69b568f72637dc8ee459730d9c87f14a
gagan <gsgoraya@gmail.com>
2022-10-19 11:41:29 +0530

    moved account and merchant to parent scope

--- index.tsx
+++ index.tsx
@@ -4,25 +4,6 @@ import NetworksList from "./NetworksList";
 export default function NetworksTokens({store, setStore, networks}: any) : JSX.Element {
     return (
         <>
-            {/* making changes */}
-            <FormControl>
-                <FormLabel>Merchant Address</FormLabel>
-                <Input value={store.merchant || ''} onInput={(e:any) => {
-                setStore((prev: any) => {
-                    return {...prev, merchant: e.target.value};
-                })
-                }} />
-                
-            </FormControl>
-            <FormControl>
-                <FormLabel>Account ID</FormLabel>
-                <Input type="number" value={store.accountId || ''} onInput={(e:any) => {
-                setStore((prev: any) => {
-                    return {...prev, accountId: e.target.value};
-                })
-                }} />
-                <FormHelperText>The account id can be found at merchants.org</FormHelperText>
-            </FormControl>
             <NetworksList networks={networks} chains={store.chains} setStore={setStore}  />
         </>
     )
